e9561ba0a9b387a7997e82a6a4846d2112128d38
Thomas Mjåland <megabjarne@hotmail.com>
2019-05-14 23:48:57 +0200

    changed to map-based training. expanded functionality

--- main.cpp
+++ main.cpp
@@ -6,39 +6,38 @@
 #include<cstdlib>
 #include<time.h>
 #include<cstring>
+#include<map>
 
 using std::cout;
 using std::cerr;
 using std::ifstream;
 using std::string;
 using std::stringstream;
+using std::map;
 
 const char *values = "abcdefghijklmnopqrstuvwxyz., \n?!:;-\"";
 int valuecount = 36;
-int charcount = 4;
+int memorycount = 6;
+bool lowercasing = false;
+bool verbose = false;
 
-uint16_t **tdata;
+map<string, map<char, uint32_t> > rules;
 
 char translate(int v){
 	return values[v];
 }
 
-int translate(char* c){
-	int v=0;
-	for (int i=0; i<charcount; i++){
-		v = v*valuecount + c[i];
-	}
-	return v;
-}
-
 int translate(char c){
-	if (c >= 'A' && c <='Z')
+	if (lowercasing && c >= 'A' && c <='Z')
 		c = (c-'A') + 'a';
-	for (int i=0; i<valuecount; i++){
+	for (int i=0; i<valuecount; i++)
 		if (values[i] == c)
 			return i;
-	}
 	return -1;
+}
+
+bool valid(char c){
+	return translate(c) != -1;
 }
 
 int pow(int n, int e){
@@ -49,43 +48,72 @@ int pow(int n, int e){
 	return v;
 }
 
-void train(string data){
-	cout<<"clearing memory\n";
-	for (int i=0;i<pow(valuecount, charcount); i++){
-		for (int j=0; j<valuecount; j++){
-			tdata[i][j] = 0;
-		}
-	}
-	cout<<"clearing done\n";
-	cout<<"training starting\n";
-
-	char *currentpos = new char[charcount];
-	for (int i=0; i<charcount; i++){
-		currentpos[i] = 0;
-	}
-	int index, cindex;
+string clean(string data){
+	string result = "";
 	for (int i=0; i<data.size(); i++){
-		index = translate(currentpos);
-		cindex = translate(data[i]);
-		if (cindex >= 0){
-			tdata[index][translate(data[i])]++;
-			for (int j = 0; j<charcount-1; j++){
-				currentpos[j] = currentpos[j+1];
-			}
-			currentpos[charcount-1] = translate(data[i]);
+		if (valid(data[i])){
+			result += data[i];
 		}
 	}
-	for (int i=0; i<pow(valuecount, charcount); i++){
-		for (int j=0; j<valuecount; j++){
-			//tdata[i][j] = tdata[i][j]*tdata[i][j];
-		}
-	}
-	delete currentpos;
-	cout<<"training done\n";
+	return result;
 }
 
-void generate(int count){
-	char *current = new char[charcount];
+void train(string data){
+	for (int i=0; i<data.size()-valuecount; i++){
+		string index = data.substr(i, memorycount-1);
+		char indexc = data[i+memorycount-1];
+		
+		auto iter = rules.find(index);
+		if (iter == rules.end()){
+			rules[index] = map<char, uint32_t>();
+			iter = rules.find(index);
+		}
+		map<char, uint32_t> &m = iter->second;
+		auto it = m.find(indexc);
+		if (it == m.end()){
+			m[indexc] = 1;
+		}else{
+			(it->second)++;
+		}
+	}
+}
+
+string generate(int count){
+	string resultstring = "";
+	string index = "";
+	{
+		// select random index string from ruleset
+		int indexindex = (rand() % rules.size());
+		auto it = rules.begin();
+		for (int i=0; i<indexindex; i++){
+			it++;
+		}
+		index = it->first;
+	}
+	if (verbose)
+		cout<<"generate seed: "<<index<<"\n";
+
+	cout<<"generated: ";
+	for (int i=0; i<count; i++){
+		auto charmap = rules[index];
+		int sum = 0;
+		for (auto it = charmap.begin(); it != charmap.end(); it++){
+			sum += it->second;
+		}
+		int randval = rand() % sum;
+		for (auto it = charmap.begin(); it != charmap.end(); it++){
+			randval -= it->second;
+			if (randval < 0){
+				resultstring += it->first;
+				index = index.substr(1) + it->first;
+				cout<<it->first;
+				break;
+			}
+		}
+	}
+	return resultstring;
+
+/*	char *current = new char[charcount];
 	for (int i=0; i<charcount; i++){
 		current[i] = rand()%valuecount;
 	}
@@ -121,16 +149,29 @@ void generate(int count){
 		}
 	}
 	delete current;
+*/}
+
+void print_basic_usage(const char *executablename){
+	cout<<	"BASIC USAGE: "<<executablename<<" -f [filename]\n";
 }
-
-void print_usage(){
-
+void print_usage(const char *executablename){
+	print_basic_usage(executablename);
+	cout<<"\n"<<
+			"    -f [filename]                  file to be used for training data.\n"<<
+			"    -v | --verbose                 extra debug logging.\n"<<
+			"    -c | --characters [characters] set of characters allowed.\n"<<
+			"    -l [number]                    how many characters to generate.\n"<<
+			"    --lowercase                    converts all characters to lowercase.\n"<<
+			"    -m [size]                      how many characters to take into consideration.\n"<<
+			"    -h | --help                    show this help message.\n"<<
+			"\n";
 }
 
 int main(int argn, const char** args){
-	const char* filename = "\0";
+	srand(time(NULL));
+	const char* filename = nullptr;
 	int verbosity = 0;
-	int generatelength = 8192;
+	int generatelength = 1000;
 	// parse arguments
 	int index = 1;
 	while (index < argn){
@@ -138,18 +179,20 @@ int main(int argn, const char** args){
 			index++;
 			if (index == argn){
 				cerr<<"missing argument [filename] for -f flag\n";
-				return 1;
+				print_usage(args[0]);
+				exit(1);
 			}
 			filename = args[index];
 		}
-		if (strcmp(args[index], "-v") == 0){
-			verbosity++;
+		if (strcmp(args[index], "-v")==0 || strcmp(args[index], "--verbose")==0){
+			verbose = true;
 		}
-		if (strcmp(args[index], "-c") == 0){
+		if (strcmp(args[index], "-c")==0 || strcmp(args[index], "--characters")==0){
 			index++;
 			if (index == argn){
 				cerr<<"missing argument [character set] for -c flag\n";
-				return 1;
+				print_usage(args[0]);
+				exit(1);
 			}
 			values = args[index];
 			valuecount = strlen(values);
@@ -158,48 +201,62 @@ int main(int argn, const char** args){
 			index++;
 			if (index == argn){
 				cerr<<"missing argument [generation length] for -l flag\n";
-				return 1;
+				print_usage(args[0]);
+				exit(1);
 			}
 			generatelength = atoi(args[index]);
 		}
+		if (strcmp(args[index], "--lowercase") == 0){
+			lowercasing = true;
+		}
+		if (strcmp(args[index], "-m") == 0){
+			index++;
+			if (index == argn){
+				cerr<<"missing argument [memory size] for -m flag \n";
+				print_usage(args[0]);
+				exit(1);
+			}
+			memorycount = atoi(args[index]);
+		}
+		if (strcmp(args[index], "-h")==0 || strcmp(args[index], "--help")==0){
+			print_usage(args[0]);
+			exit(0);
+		}
 		index++; 
 	}
+	
+	if (filename == nullptr){
+		cerr<<"Input file must be specified\n";
+		print_basic_usage(args[0]);
+		exit(0);
+	}
 
-	srand(time(NULL));
+	// read in training data
 	ifstream file = ifstream(filename);
 	if (!file.good()){
 		cerr<<"unable to open/read file '"<<filename<<"'\n";
-		return -1;
+		exit(2);
 	}
 	
-	if (verbosity >= 1)
+	if (verbose)
 		cout<<"reading file\n";
 	
 	stringstream buffer;
 	buffer << file.rdbuf();
 	
-	if (verbosity >= 1)
+	if (verbose)
 		cout<<"read "<<buffer.str().size()<<" characters\n";
 
-	if (verbosity >= 1)
-		cout<<"allocating training memory ("<< pow(valuecount, charcount)*sizeof(uint16_t*) + pow(valuecount, charcount)*valuecount*sizeof(uint16_t) <<"B)\n";
+	string data = clean(buffer.str());
 
-	tdata = new uint16_t*[pow(valuecount, charcount)];
-	
-	for (int i=0; i<pow(valuecount, charcount); i++){
-		tdata[i] = new uint16_t[valuecount];
-	}
+	if (verbose)
+		cout<<data.size()<<" characters left after cleaning\n";
 
-	if (verbosity >= 1)
-		cout<<"allocation finished\n";
-
-	train(buffer.str());
+	train(data);
 	
-	generate(generatelength);
-	cout<<"\n";
+	string result = generate(generatelength);
 	
-	for (int i=0; i<pow(valuecount, charcount); i++){
-		delete tdata[i];
-	}
-	delete tdata;
+	cout<<result<<"\n";
+	
+	exit(0);
 }
